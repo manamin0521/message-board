8df15da8f0d2d0d842ed9aa1b5fc8c0b28e1d96a
manamin0521 <hr.manami.0521@gmail.com>
2017-03-05 06:13:54 +0000

    Fixed age

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20 } , presence: true
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
-    validates :age , length: { minimum: 0, maximum: 150 }, numericality: true
+    validates :age , :inclusion => { :in => 0..150 }, numericality: true
 end
